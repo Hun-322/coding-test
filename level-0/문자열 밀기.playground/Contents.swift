9c0d7008d545a0a81ce903d8d05b1489dda22d90
Hun-0322 <ksh26271@naver.com>
2022-12-10 23:59:32 +0900

    치킨 쿠폰

--- level-0/문자열 밀기.playground/Contents.swift
+++ level-0/문자열 밀기.playground/Contents.swift
@@ -1,3 +1,18 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func solution(_ A:String, _ B:String) -> Int {
+    var s = 0
+    if A == B {
+        return 0
+    }
+    if let rangeS = (B+B).range(of: A) {
+    s = (B+B).distance(from: (B+B).startIndex, to: rangeS.lowerBound)
+}
+    return s == 0 ? -1 : s
+}
+
+// 다른사람의 모범 답안
+func goodSolution(_ A:String, _ B:String) -> Int {
+    let arr = (A+A).components(separatedBy: B)
+    return arr.count == 1 ? -1 : arr[1].count
+}
