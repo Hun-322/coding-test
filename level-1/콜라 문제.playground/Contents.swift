a028ff3e99174db56972d6e7869363abb7ebbd2b
Hun-0322 <ksh26271@naver.com>
2022-12-25 02:05:58 +0900

    콜라 문제

---
+++ level-1/콜라 문제.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
+    var n = n
+    var answer = 0
+    
+    while n >= a {
+        answer += n / a * b
+        n = (n % a) + (n / a * b)
+    }
+    return answer
+}
